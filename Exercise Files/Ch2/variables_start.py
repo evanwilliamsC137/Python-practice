b70faa3c267b3753f70357ec052a37882712f70c
Evan Williams <evan.b.williams95@gmail.com>
2021-09-17 12:54:32 -0500

    hello world and basic variables

---
+++ Exercise Files/Ch2/variables_start.py
@@ -0,0 +1,32 @@
+# 
+# Example file for variables
+#
+
+# Declare a variable and initialize it
+
+f=0
+print(f)
+
+
+# re-declaring the variable works
+
+f="abc"
+print(f)
+
+
+# ERROR: variables of different types cannot be combined
+
+# print("This is a string" + 123) doesn't work
+print("This is a string " + str(123)) # does work
+
+
+# Global vs. local variables in functions
+
+def someFunction():
+    global f
+    f="def"
+    print(f)
+
+someFunction()
+print(f)
+
