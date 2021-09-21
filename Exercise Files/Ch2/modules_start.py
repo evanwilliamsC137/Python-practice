69f1c6323bc8234018110264523968412e1ee78c
Evan Williams <evan.b.williams95@gmail.com>
2021-09-21 13:01:27 -0500

    modules

--- Exercise Files/Ch2/modules_start.py
+++ Exercise Files/Ch2/modules_start.py
@@ -1,10 +1,23 @@
 # import the math module, which contains features for working with mathematics
 
+import math
+
 
 # the math module contains lots of pre-built functions
 
+print("The square root of 16 is ", math.sqrt(16))
 
 # in addition to functions, some modules contain useful constants
 
+print("PI is ", math.pi)
+
 
 # try some of the math functions for yourself here:
+
+print("2 + 2 is ",(2 + 2))
+
+print(math.cos(1/3))
+
+print(math.factorial(5))
+
+print(math.e)
