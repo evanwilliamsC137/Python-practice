49cb5feffd029977f0ae16f041751ea50de64efc
Evan Williams <evan.b.williams95@gmail.com>
2021-09-23 14:25:54 -0500

    xml parsing

--- Exercise Files/Ch5/htmlparsing_start.py
+++ Exercise Files/Ch5/htmlparsing_start.py
@@ -1,28 +1,28 @@
-# 
-# Example file for parsing and processing HTML
+# #
+# # Example file for parsing and processing HTML
+# #
+# from html.parser import HTMLParser
 #
-from html.parser import HTMLParser
-
-class MyHTMLParser(HTMLParser):
-  def handle_comment(self, data):
-    print("Encountered comment: ", data)
-    pos = self.getpos()
-    print("\tAt line: ", pos[0], " position ", pos[1])
-
-def handle_starttag(self, tag, attrs):
-
-def handle_endtag(self, tag):
-
-def handle_data(self, data)
-def main():
-  # instantiate the parser and feed it some HTML
-  parser = MyHTMLParser()
-  f = open("samplexml.xml")
-  if f.mode == 'r':
-    contents = f.read()
-    parser.feed(contents)
-    
-
-if __name__ == "__main__":
-  main();
-  
+# class MyHTMLParser(HTMLParser):
+#   def handle_comment(self, data):
+#     print("Encountered comment: ", data)
+#     pos = self.getpos()
+#     print("\tAt line: ", pos[0], " position ", pos[1])
+#
+# def handle_starttag(self, tag, attrs):
+#
+# def handle_endtag(self, tag):
+#
+# def handle_data(self, data)
+# def main():
+#   # instantiate the parser and feed it some HTML
+#   parser = MyHTMLParser()
+#   f = open("samplexml.xml")
+#   if f.mode == 'r':
+#     contents = f.read()
+#     parser.feed(contents)
+#
+#
+# if __name__ == "__main__":
+#   main();
+#
